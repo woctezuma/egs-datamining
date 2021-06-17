f3ac99f776a3040e99f9d7431de5c5bc43586910
Wok <woctezuma@users.noreply.github.com>
2021-06-17 15:26:58 +0200

    Add utils to locally load/save item data for specified item ids

---
+++ src/item_data_utils.py
@@ -0,0 +1,26 @@
+import json
+from pathlib import Path
+
+from src.data_utils import save_data
+from src.download_utils import download_from_item_data_tracker
+
+
+def get_item_data_filename(item_id, folder_name="data/items/"):
+    Path(folder_name).mkdir(parents=True, exist_ok=True)
+
+    fname = f"{item_id}.json"
+
+    return folder_name + fname
+
+
+def load_item_data(item_id):
+    fname = get_item_data_filename(item_id)
+
+    try:
+        with open(fname, encoding="utf8") as f:
+            item_data = json.load(f)
+    except FileNotFoundError:
+        item_data = download_from_item_data_tracker(item_id)
+        save_data(item_data, fname)
+
+    return item_data
